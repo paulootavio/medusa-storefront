6f05346a3e832db8c131a0f9ae8dcf58ccfbee43
paulo.otavio <paulo.otavio@esx.com.br>
2024-01-28 13:29:04 -0300

    firts commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', 'nuxt-medusa', '@nuxtjs/google-fonts'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  },
+  googleFonts: {
+    families: {
+      Inter: [300, 400, 500, 600, 700, 800, 900]
+    }
+  },
+  devServer: {
+    port: 8000
+  },
+})
